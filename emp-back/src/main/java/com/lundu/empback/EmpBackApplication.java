4a603d5ab5ce6221d70ca3124a78544e82ed714b
samson28 <58706301+samson28@users.noreply.github.com>
2023-06-19 15:01:21 +0000

    initialise backend in Spring Boot

---
+++ emp-back/src/main/java/com/lundu/empback/EmpBackApplication.java
@@ -0,0 +1,13 @@
+package com.lundu.empback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpBackApplication.class, args);
+	}
+
+}
---
+++ emp-back/src/test/java/com/lundu/empback/EmpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lundu.empback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
