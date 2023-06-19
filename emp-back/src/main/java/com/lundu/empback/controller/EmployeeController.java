455956585cc4d705e6cf944761769f307edc4c76
samson28 <58706301+samson28@users.noreply.github.com>
2023-06-19 16:57:45 +0000

    limit access to user

--- emp-back/src/main/java/com/lundu/empback/controller/EmployeeController.java
+++ emp-back/src/main/java/com/lundu/empback/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,42 +19,42 @@ public class EmployeeController {
     EmployeeService employeeService;
 
     @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
-    //@PreAuthorize("hasAuthority('SCOPE_USER')")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public ResponseEntity<List<Employee>> getAllEmployee(){
         List<Employee> employees = employeeService.showAll();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
     @GetMapping(value = "/show/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    //@PreAuthorize("hasAuthority('SCOPE_USER')")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public ResponseEntity<Employee> getOneEmployee(@PathVariable("id") int id){
         Employee employee = employeeService.find(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 
     @PostMapping(value = "/store", consumes = MediaType.APPLICATION_JSON_VALUE)
-    //@PreAuthorize("hasAuthority('SCOPE_USER')")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public ResponseEntity<Employee> addOneEmployee(@RequestBody Employee employee){
         employeeService.save(employee);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_VALUE)
-    //@PreAuthorize("hasAuthority('SCOPE_USER')")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
         employeeService.save(employee);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping(value = "/search/{tips}", produces = MediaType.APPLICATION_JSON_VALUE)
-    //@PreAuthorize("hasAuthority('SCOPE_USER')")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public ResponseEntity<List<Employee>> searchEmployee(@PathVariable("tips") String tips){
         List<Employee> employees = employeeService.search(tips);
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
-    //@PreAuthorize("hasAuthority('SCOPE_USER')")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public ResponseEntity<Employee> deleteEmployee(@PathVariable("id") int id){
         employeeService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
